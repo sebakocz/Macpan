9e3f0b1af62de8dc47db74142398eac47a6b15fb
Orbiter1 <orbiter14@gmail.com>
2019-05-06 16:29:15 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import static org.lwjgl.glfw.GLFW.*;
+
 
 public class Main {
 
     public Main() {
-        if (!glfwInit()) {
-            System.err.println("GLFW Failed to initialize!");
-        }
+
     }
 
     public static void main(String[] args) {
