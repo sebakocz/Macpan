3e1ad5158f69de69d10d4d1eb38a245a7be56630
Orbiter1 <orbiter14@gmail.com>
2019-05-21 11:40:52 +0200

    Map is loaded Todo: PacMan movement system

--- src/com/bensep/macpan/gameWorld/PacManGrid.java
+++ src/com/bensep/macpan/gameWorld/PacManGrid.java
@@ -8,18 +8,49 @@ import com.bensep.macpan.myGameLib.GameObject;
 import com.bensep.macpan.myGameLib.GameWorld;
 import com.bensep.macpan.tesxtures.Textures;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static com.bensep.macpan.constants.Constants.*;
 
 public class PacManGrid extends GameWorld {
 
     public PacManGrid() {
         super(WORLD_WIDTH, WORLD_HEIGHT, TILE_SIZE);
-        for (int x = 0; x < WORLD_WIDTH; x++) {
+        loadGrid();
+        entities.add(new PacMan(13.5f,9,this));
+    }
+
+    public void loadGrid() {
+        try {
+            Scanner scanner = new Scanner(new File("res/Level/Level1"));
             for (int y = 0; y < WORLD_HEIGHT; y++) {
-                setGridAt(x,y,false, Textures.getInstance().grid[6][0]);
+                String[] strings = scanner.nextLine().split(",");
+                for (int x = 0; x < WORLD_WIDTH; x++) {
+                    switch (strings[x]) {
+                        case "pallet":
+                            setGridAt(x,y,false, Textures.getInstance().empty);
+                            break;
+                        case "energizer":
+                            setGridAt(x,y,false, Textures.getInstance().empty);
+                            break;
+                        case "70":
+                            setGridAt(x,y,false, Textures.getInstance().empty);
+                            break;
+                        case "71":
+                            setGridAt(x,y,false, Textures.getInstance().barrier);
+                            break;
+                            default:
+                                setGridAt(x,y,true, Textures.getInstance().grid[Integer.valueOf(strings[x])/10][Integer.valueOf(strings[x])%10]);
+                                break;
+                    }
+                }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        entities.add(new PacMan(8,8,this));
+
     }
 
     @Override
